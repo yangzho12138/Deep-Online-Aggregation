915aee9797b41dee1bb0289da6c40e75cc708ab9
kfadillah <fadillahmichael0@gmail.com>
2022-09-30 16:01:42 -0500

    install pyarrow

--- deepola/wake/examples/tpch_polars/test.py
+++ deepola/wake/examples/tpch_polars/test.py
@@ -1,9 +1,16 @@
 from tpch_polars import *
 import numpy as np
 import pandas as pd
+import polars as pl
+
+import io
 
 result = run_thread("q1",1, "../../resources/tpc-h/data/scale=0.05/partition=5")
 
-df = pd.DataFrame(data=result)
 
-print(df)
+for record in result:
+    buf = io.StringIO(record)
+    tmp = pl.read_json(buf)
+    df = tmp.to_pandas()
+    print(df)
+
