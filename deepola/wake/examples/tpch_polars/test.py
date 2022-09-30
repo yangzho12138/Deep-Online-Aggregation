b3dca23ab9673c7ad86ff1f1faf0d298ab9b55c6
kfadillah <fadillahmichael0@gmail.com>
2022-09-30 15:27:32 -0500

    small fix

--- deepola/wake/examples/tpch_polars/test.py
+++ deepola/wake/examples/tpch_polars/test.py
@@ -2,7 +2,7 @@ from tpch_polars import *
 import numpy as np
 import pandas as pd
 
-result = run_query(["q1","1", "../../resources/tpc-h/data/scale=0.05/partition=5"])
+result = run_thread(["q1","1", "../../resources/tpc-h/data/scale=0.05/partition=5"])
 
 df = pd.DataFrame(data=result)
 
