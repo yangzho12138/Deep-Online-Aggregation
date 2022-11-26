bc5daeda4d7c4b00b0f45902d3fdfaf4159b31c5
yz96 <yz96@fa22-cs511-004.cs.illinois.edu>
2022-11-25 20:19:20 -0600

    py convert to json

---
+++ scripts/convert-to-json.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import glob
+import polars as pl
+from concurrent.futures import ThreadPoolExecutor
+
+# Converts the filename from: %x.tbl.1 to %x.json.1
+def convert_file_to_json(file_name):
+    target_file_name = file_name.replace('tbl','json')
+    if os.path.exists(target_file_name):
+        print(f"File {target_file_name} already exists")
+        return target_file_name
+    df = pl.read_csv(file_name, has_header = False, sep = "|")
+    df.write_json(target_file_name)
+    return target_file_name
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 convert_to_parquet.py <input-dir. ex: ../sresources/tpc-h/data/scale=10/partition=10/tbl/>")
+        exit(1)
+    input_directory = sys.argv[1]
+
+    # Obtain the files that need to be converted
+    files = glob.glob(os.path.join(input_directory,"*.tbl*"))
+
+    output_directory = input_directory.replace("tbl","json")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory, exist_ok=True)
+
+    # Convert each file to json
+    print(f"Found {len(files)} files")
+    for file_name in files:
+        print(f"Converting {file_name}")
+        convert_file_to_json(file_name)
